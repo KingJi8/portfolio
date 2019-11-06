9ff9caa010e4ae7736f45904375ae7f8470b6ea6
KingJi <jiwon.c.king@gmail.com>
2019-11-06 15:42:23 +0900

    about_20191106-1542

--- js/about.js
+++ js/about.js
@@ -16,38 +16,6 @@ $(function(){
     }
     tbackH();
     
-//    /*top sec 원형 높이*/
-//    function tTcircleH() {
-//        var tTCirW = $('.t-t-c-text').outerWidth();
-//        $('.t-t-c-text').height(tTCirW);
-//        
-//        var tH = $('.t-t-c-text').outerHeight();
-//        var typoH = $('.t-typo-sec').outerHeight();
-//        var thTop = (typoH-tH)/2
-//        $('.t-t-c-text').css({top:thTop})
-//    }
-//    
-//    /*top 원형 속 글자*/
-//    function ttctext() {
-//        var tH = $('.t-t-c-text').height();
-//        var tdefaultH = $('.ttc-default').height();
-//        var ttctxtH = $('.t-t-c-0').height();
-//        $('.t-t-c-text').find('.ttc-default').css({top:(tH-tdefaultH)/2+'px'});
-//        $('.t-typo').each(function(i){
-//            $(this).on('mouseenter',function(){
-//                $(this).css({transition:'.6s',color:'#3cb54a',});
-//                $('.t-t-c-text').find('.t-t-c-'+i).css({top:(tH/2)-(ttctxtH+3)},du);
-//                $('.t-t-c-text').find('.ttc-default').stop().animate({top:(tH/2)+3,},du,function(){                    
-//                    $('.t-t-c-text').find('.t-t-c-'+i).stop().animate({opacity:1},du);
-//                });
-//            }).on('mouseleave',function(){
-//                $(this).css({transition:'.6s',color:'#ffffff',});
-//                $('.t-t-c-text').find('.t-t-c-'+i).stop().animate({opacity:0,},du);
-//                $('.t-t-c-text').find('.ttc-default').stop().animate({top:(tH-tdefaultH)/2,},du);
-//            })
-//        })
-//    }
-    
     /*contents*/
     /*skill 원형*/    
     function skillCir() {
@@ -72,32 +40,6 @@ $(function(){
         })
     }
     skillCir();
-    /*var sCirW = $('.skill').width();
-    $('.skill').height(sCirW);
-    
-    var skillnum = [];
-    var count = 0;
-    $('.skill').each(function(i){
-        skillnum[i] = $(".skill").eq(i).find(".s-num").text();
-        //console.log(skillnum);
-        $(window).scroll(function(){
-            var winS = $(window).scrollTop();
-            var sTop = $('.skillSec').offset().top;
-            
-            if(winS>=sTop-(winH*.8)) {
-                var sCount = setInterval(sCir,70);
-                function sCir(){
-                    if(count>=skillnum[i]){
-                        clearInterval(sCount);
-                    }else {
-                        count ++;
-                        $('.skill').eq(i).find('.s-txt').css({background:'conic-gradient(#333333 0% '+count+'%, transparent '+count+'% 100%)'})
-                    };
-                };
-                $(".skill").eq(i).find(".s-num").text(skillnum[i]);
-            }
-        })
-    })*/
     
     /*skill box Height & skill button*/
     function skillBtn() {
@@ -150,26 +92,6 @@ $(function(){
         })
     }
     lifeMove();
-    
-    /*more about*/
-    /*more 답 펴지기
-    $('.m-question').each(function(){
-        $(this).find('.m-q-question').on('click',function(){
-            if($(this).hasClass('active') == false) {
-                $('.m-q-answer').slideUp();
-                $('.m-q-answer, .m-q-question').removeClass('active');
-                $(this).next('.m-q-answer').slideDown(du);
-                $(this).next('.m-q-answer').addClass('active');
-                $(this).addClass('active');
-            }else {
-                $('.m-q-answer').slideUp();
-                $('.m-q-answer, .m-q-question').removeClass('active');
-            }
-            
-        })
-    })*/
-    
-    
     
     
     $(window).resize(function(){
